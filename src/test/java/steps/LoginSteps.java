8e2e7affd56fd3499fdb4731928c980c4b923a36
avismm <amuhwerane@gmail.com>
2023-11-06 06:26:30 +0700

    Group1 final draft skepton

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -8,10 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import utils.CommonMethods;
-import utils.ConfigReader;
-
-import java.time.Duration;
 
 public class LoginSteps extends CommonMethods {
 
