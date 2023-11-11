2529bd24019f6954a2cb3c0d01ae03aa751bdfaf
laasri1990 <laasrii2013@gmail.com>
2023-11-10 20:02:59 -0800

    my final work on user story

--- src/test/java/utils/PageInitializer.java
+++ src/test/java/utils/PageInitializer.java
@@ -1,19 +1,18 @@
 package utils;
 
-import pages.AddEmployeePage;
-import pages.DashboardPage;
-import pages.EmployeeSearchPage;
-import pages.LoginPage;
+import pages.*;
 
 public class PageInitializer {
     public static LoginPage loginPage;
     public static DashboardPage dashboardPage;
     public static EmployeeSearchPage employeeSearchPage;
     public static AddEmployeePage addEmployeePage;
+    public static membershipSectionPage membershipSectionPage;
     public static void initializePageObjects(){
         loginPage= new LoginPage();
         dashboardPage= new DashboardPage();
         employeeSearchPage= new EmployeeSearchPage();
         addEmployeePage= new AddEmployeePage();
+        membershipSectionPage=new membershipSectionPage();
     }
 }
