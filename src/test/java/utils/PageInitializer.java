6f2abeeca5afa80b58a5bba7dfef53589be3c72c
michelleh30 <mdhurtad15@gmail.com>
2023-11-11 10:32:58 -0500

    michelle first commit to branch

--- src/test/java/utils/PageInitializer.java
+++ src/test/java/utils/PageInitializer.java
@@ -1,19 +1,23 @@
 package utils;
 
-import pages.AddEmployeePage;
-import pages.DashboardPage;
-import pages.EmployeeSearchPage;
-import pages.LoginPage;
+import pages.*;
 
 public class PageInitializer {
+    //created the obj
     public static LoginPage loginPage;
     public static DashboardPage dashboardPage;
     public static EmployeeSearchPage employeeSearchPage;
     public static AddEmployeePage addEmployeePage;
+    public static AdminPage adminPage;
+    public static LanguagePage languagePage;
+
     public static void initializePageObjects(){
+        //calling the obj
         loginPage= new LoginPage();
         dashboardPage= new DashboardPage();
         employeeSearchPage= new EmployeeSearchPage();
         addEmployeePage= new AddEmployeePage();
+        adminPage= new AdminPage();
+        languagePage= new LanguagePage();
     }
 }
